1b8314feeaba453b3ecd68b902936461879c1f94
ramirochangomoreno <rama.exe.13@gmail.com>
2023-07-21 15:28:27 -0300

    lnk loge

--- src/app/landing/component/NavBar.jsx
+++ src/app/landing/component/NavBar.jsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 import React from "react";
 import SVGLogin from "./SVGComponent";
 import logo from "../../../../public/logo.png";
+import LinkLogin from "./LinkLogin";
 
 export default function NavBar() {
   return (
     <nav className="h w-screen py-2 fixed top-0 left-0  flex items-center bg-primary-500 justify-between px-12 text-primary-200 z-40">
-      <div className="w-1/4 flex items-center justify-between font-light text-sm">
+      <div className="w-1/3 flex items-center justify-between font-light text-sm">
         <Image alt="logo"
         src={logo}
         width={50}
@@ -21,10 +22,7 @@ export default function NavBar() {
         <li>Aviso de Privacidad</li>
         <li>Contacto</li>
       </ul>
-      <div className="w- flex flex-col items-center justify-center text-sm">
-        <SVGLogin className="w-10" />
-        <span className="w-fll">Iniciar Sesion</span>
-      </div>
+    <LinkLogin/>
     </nav>
   );
 }
