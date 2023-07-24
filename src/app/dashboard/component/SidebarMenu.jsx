58cf96f245df6288b587323645475cd3c92f9114
ramirochangomoreno <rama.exe.13@gmail.com>
2023-07-24 09:50:31 -0300

    sidebar flex grow

--- src/app/dashboard/component/SidebarMenu.jsx
+++ src/app/dashboard/component/SidebarMenu.jsx
@@ -14,10 +14,10 @@ const params=usePathname()
   return (
     <Link
       href={path}
-      className={` ${params==path? "text-primary-100":" text-primary-textGris"} flex items-center justify-normal gap-3 hover:text-primary-100 font-semibold group cursor-pointer `}
+      className={` ${params==path? "text-primary-100 fill-primary-100":"fill-primary-textGris text-primary-textGris"} flex items-center justify-normal gap-3 hover:text-primary-100 font-semibold group cursor-pointer `}
     >
-      <div className="w-5 h-5">{icon}</div>
-      <div className="text-sm">{name}</div>
+      <div className="text-xs lg:text-sm w-5 h-5">{icon}</div>
+      <div className="text-xs">{name}</div>
     </Link>
   );
 };
@@ -38,14 +38,14 @@ export default function SidebarMenu() {
           name: `Driver ${i}`,
           path: `/dashboard/driver/${i}`,
           icon: (
-            <SVGPantalla className="w-5 h-5  group-hover:fill-primary-100 fill-primary-textGris" />
+            <SVGPantalla className="w-5 h-5   group-hover:fill-primary-100 fill-primary-textGris" />
           ),
           id: i + 1,
         });
       }
       
   return (
-    <div className="flex flex-col flex-grow items-start justify-center gap-7 w-10/12 mx-auto pb-5">
+    <div className="flex flex-col flex-shrink items-start justify-center gap-5 w-10/12 mx-auto pb-5">
       {links?.map((link) => (
         <LinksSidebarDachboard
           name={link.name}
