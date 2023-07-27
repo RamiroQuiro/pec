e104ae120887f8ee5407a77704b9e56ee95f951d
ramiroquiro <rama.exe.13@gmail.com>
2023-07-27 14:23:41 -0300

    cargando nextauth

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials";
+const handler = NextAuth({
+
+    providers:[
+        CredentialsProvider({
+            name:'credentials',
+            credentials:{
+                email:{
+                    label:"Email",type:"text",placeholder:"email"
+                },
+                password:{
+                    label:"password",type:"password"
+                },
+            },
+             authorize(credentials,req){
+                const user={id:1,fullName:"",email:"rama.exe@fmail.com"}
+                return user
+            }
+        })
+    ]
+
+})
+
+export { handler as GET, handler as POST }
