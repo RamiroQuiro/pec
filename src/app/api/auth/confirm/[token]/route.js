aaa05a5b45d91a326083dc192159d9dba6c96e07
ramiroquiro <rama.exe.13@gmail.com>
2023-08-01 18:04:27 -0300

    configurando ruta de confirmacion de registro

--- src/app/api/auth/confirm/[token]/route.js
+++ src/app/api/auth/confirm/[token]/route.js
@@ -1,15 +1,6 @@
 import { NextResponse } from "next/server";
-import User from "@/models/user";
-import { connectDB } from "@/libs/mongoodb";
-import { v4 as uuidv4 } from 'uuid';
-import bcrypt from "bcryptjs";
-import { getToken } from "@/libs/jwt";
-import { getTemplate, sendMailer } from "@/libs/nodemailer";
 
-export async function GET(request) {
-//   const { token} = await request.json();
-console.log(token)
-  return NextResponse.json({
-    message:'token recibido'
-  })
+export function GET(request,{params}) {
+    console.log(params.token)
+  return NextResponse.json({message: "ramiro"});
 }
