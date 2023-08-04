5d8d5b1fa8f4632134effe4ce55989e781cb240b
ramiroquiro <rama.exe.13@gmail.com>
2023-08-04 01:18:12 -0300

    realizando los pagos

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -1,34 +1,23 @@
 import Stripe from "stripe"; 
 import { NextResponse } from "next/server";
 
-const stripe=new Stripe(process.env.STRIPE_SECRET_KEY)
-export async function GET(){
-    
-
-const product=await stripe.prices.list()
-
-
-    // const session=await stripe.checkout.sessions.create({
-    //     line_items:[
-    //         {
-    //             price_data:{
-    //                 currency: 'usd',
-    //                 product_data:{
-    //                     name:'my-product',
-    //                     description:"mi producto",
-    //                 },
-    //                 unit_amount:20000,
-    //             },
-    //             quantity:5
-    //         },
+export async function POST(request){
+    const stripe=new Stripe(process.env.STRIPE_SECRET_KEY)
+    const {priceId}=await request.json()
+    const session=await stripe.checkout.sessions.create({
+        payment_method_types:['card'],
+        line_items:[
+            {
+               price:priceId,quantity:1
+            },
          
-    //     ],
-    //     mode:'payment',
-    //     success_url:`https://localhost:3000/pagoCorrecto`,
-    //     cancel_url:`https://localhost:3000/pagoIncorrecto`
-    // },
+        ],
+        mode:'payment',
+        success_url:`https://localhost:3000/pagoCorrecto`,
+        cancel_url:`https://localhost:3000/pagoIncorrecto`
+    },
     
     
-    // )
-    return NextResponse.json(product.data);
+    )
+    return NextResponse.json(session);
 }
--- src/app/pricepec/page.jsx
+++ src/app/pricepec/page.jsx
@@ -10,19 +10,11 @@ import {
 import Footer from "../landing/Footer";
 import Image from "next/image";
 import logo from "../../../public/logo.png";
+import CardPrice from "./CardPrice";
 
-import Stripe from "stripe"; 
 
- async function getDataStripe(){
-  const stripe=new Stripe(process.env.STRIPE_SECRET_KEY)
-const product=await stripe.prices.list()
+export default  function PricePEC() {
 
-return product.data
- }
-
-export default async function PricePEC() {
-const prices=await getDataStripe()
-  console.log(prices)
   return (
     <main className=" w-screen min-h-screen h-full flex flex-col items-center justify-between text-primary-textGris relative">
       <nav className="w-full absolute z-40 flex items-center justify-between bg-primary-tonoBlanco h-20 px-16 top-0 left-0">
@@ -52,49 +44,7 @@ const prices=await getDataStripe()
             Ultima Actualización : 19/07/2023
           </p>
         </div>
-        <div className="md:w-[450px] h-[850px] bg-white shadow-lg  absolute right-44 top-20 mt-20 border">
-          <div className="flex flex-col items-start justify-center">
-            <div className="h-[230px] bg-gray-700 w-full p-2">
-              <video src=""></video>
-            </div>
-            <div className="flex flex-col items-start mt-7 justify-center px-7 gap-4 w-full">
-              <p className="text-2xl font-black text-[#000028] tracking-wide">
-                2,500 MXN
-              </p>
-              <button className="bg-primary-100 hover:bg-primary-200 duration-500 text-white font-bold capitalize w-full py-4">
-                Comprar Ahora
-              </button>
-              <span className="text-xs text-center w-full">
-                Garantía de reembolso de 10 dias
-              </span>
-              <div>
-                <h3 className="text-lg font-bold my-5 text-[#000028] tracking-wide">
-                  Tu PEC incluye:
-                </h3>
-                <ul className="w-full flex flex-col items-start justify-center font- gap-4">
-                  <li className="w-full flex items-center justify-start  gap-2 hover:gap-4 duration-300">
-                    <SVGPantalla className="w-5" />
-                    <p>Drivers dinámicos.</p>
-                  </li>
-                  <li className="w-full flex items-center justify-start  gap-2 hover:gap-4 duration-300">
-                    <SVGInfinity className="w-5" />
-                    <p>Acceso de por vida</p>
-                  </li>
-                  <li className="w-full flex items-center justify-start  gap-2 hover:gap-4 duration-300">
-                    <SVGGit className="w-5" /> <p>Guia de uso de plataforma</p>
-                  </li>
-                  <li className="w-full flex items-center justify-start  gap-2 hover:gap-4 duration-300">
-                    <SVGDownload className="w-5 h-5" />{" "}
-                    <p>Recursos descargables</p>
-                  </li>
-                  <li className="w-full flex items-center justify-start  gap-2 hover:gap-4 duration-300">
-                    <SVGPremio className="w-5" /> <p>Certificado final</p>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+       <CardPrice/>
       </header>
       <section className="min-h-screen w-full mx-auto flex- items-center ml-32 mt-10 ">
         <div className="border shadow-sm w-1/2 p-3">
