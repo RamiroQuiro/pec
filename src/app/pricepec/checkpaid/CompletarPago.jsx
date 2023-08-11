514669c40515a0e49f52d254cb816109817829c1
Ramiro Quiroga <ramiro.quiroga@cepsi.net.ar>
2023-08-11 12:14:04 -0300

    creacion de cupones

--- src/app/pricepec/checkpaid/CompletarPago.jsx
+++ src/app/pricepec/checkpaid/CompletarPago.jsx
@@ -66,7 +66,16 @@ const aplicarDescuento=()=>{
       ) : (
         <div className=" w-full my-10 mb-20">
           <p className="text-lg font-bold mt-5">Resumen</p>
-          {prices?.map((product) => (
+          {
+          !prices?<div
+          class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status">
+          <span
+            class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+            >Loading...</span
+          >
+        </div>:
+          prices?.map((product) => (
             <div
               key={product.id}
               className="w-full  flex items-center justify-between text-xs pb-1 px-2 my-2"
