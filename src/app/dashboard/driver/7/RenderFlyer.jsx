7b9c6f4341130465b84e5deae2cc20bbf3748353
ramiroquiro <rama.exe.13@gmail.com>
2023-09-07 14:14:54 -0300

    mejorando el renderisado de los flyers

--- src/app/dashboard/driver/7/RenderFlyer.jsx
+++ src/app/dashboard/driver/7/RenderFlyer.jsx
@@ -3,41 +3,20 @@ import Image from "next/image";
 import { shallow } from "zustand/shallow";
 
 
-const Flyer = ({ flyers, flyerActivo }) => {
-  if (flyers.length === 0) {
-    // Muestra un indicador de carga o una imagen de marcador de posición
-    return  <div className="h-[100%] w-auto bg-gray-300/50 animate-pulse backdrop-blur-sm">
-   
-  </div>;
-  } else {
-    // Muestra el componente Image
-    return (
-      <div className="h-[100%] w-auto animate-[aparecer_.5s]">
-        <Image
-          alt={flyers[flyerActivo - 1]?.title}
-          src={flyers[flyerActivo - 1]?.img}
-          fill
-          quality={100}
-          className="object- object-center h-[100%] animate-[aparecer_.5s]"
-        />
-      </div>
-    );
-  }
-};
-
 export default function RenderFlyer() {
   const flyerActivo = contextUser((state) => state.flyerActivo, shallow);
-
-
-  const flyers = [];
-  for (let index = 2; index <= 17; index++) {
-    flyers.push({
-      id: index,
-      title: `Flyer ${index}`,
-      open: false,
-      img:`/driver7/Diapositiva${index}.webp`,
-    });
-  }
-
-  return <Flyer flyers={flyers} flyerActivo={flyerActivo} />;
+  return (
+    <div className="h-[100%] w-auto animate-[aparecer_.5s]">
+      <Image
+        alt={`Diapositiva${flyerActivo+1}`}
+        src={`/driver7/Diapositiva${flyerActivo+1}.webp`}
+        fill
+        quality={100}
+        className="object- object-center h-[100%] animate-[aparecer_.5s]"
+      />
+    </div>
+  );
 }
+  
+
+
